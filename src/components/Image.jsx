be53cc3534c5f1d1ead5f4899bbadd597770cf12
Alfayo01 <victoralfayo01@gmail.com>
2023-08-11 19:30:59 +0300

    Added Image Gallery component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-let img = [
+let gallery = [
     {
         url1: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fphotographycourse.net%2Fdifferent-types-of-cameras%2F&psig=AOvVaw2HNAOhK11PwQ0gslYCvafy&ust=1691660833049000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjQjOulz4ADFQAAAAAdAAAAABAE",
         width: "800px",
@@ -12,7 +12,7 @@ let img = [
 const Image = () => {
     return (
         <>
-            <img src={img.url1} width={img.width} height={img.height} alt="Picture of camera"/>
+            <img src={gallery.url1} width={gallery.width} height={gallery.height} alt="Picture of camera"/>
         </>
     );
 }
