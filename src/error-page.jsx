d021c09ee304e95e8b9c04651b7ef37f7160c325
Alfayo01 <victoralfayo01@gmail.com>
2023-08-18 20:31:31 +0300

    Changed error page status

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,20 +1,52 @@
-import {useRouteError, Link} from 'react-router-dom';
+import {useRouteError, Link, isRouteErrorResponse} from 'react-router-dom';
 
 const ErrorPage = () => {
     const error = useRouteError();
 
-    return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occured</p>
-            <p> 
-                <i>Page {error.statusText || error.message}</i>
-            </p>
-            <p>
-                Go back to <Link to="/">home page</Link>
-            </p>
-        </div>
-    );
+    if(isRouteErrorResponse(error)) {
+        if (error.status === 404) {
+                return (
+                <div id="error-page">
+                    <h1>{error.status}</h1>
+                    <h1>Oops!</h1>
+                    <p>Sorry, an unexpected error has occured</p>
+                    <p> 
+                        <i>Page {error.statusText || error.message}</i>
+                    </p>
+                    <p>
+                        Go back to <Link to="/">home page</Link>
+                    </p>
+                </div>
+            );
+        }
+        if (error.status === 401) {
+            return (
+            <div>
+                <h1>{error.status}</h1>
+                <h3>You aren't authorized to see this</h3>
+            </div>
+            );
+          }
+      
+          if (error.status === 503) {
+            return (
+            <div>
+                <h1>{error.status}</h1>
+                <h3>Looks like our API is down</h3>
+            </div>
+            );
+          }
+      
+          if (error.status === 418) {
+            return (
+            <div>
+                <h1>{error.status}</h1>
+                🫖
+            </div>
+            );
+          }
+    }
+
 }
 
 export default ErrorPage;
